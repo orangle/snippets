9e4ecd7f42dd8f62987d652c0fed433079be8b11
orangle <orangleliu@gmail.com>
2018-12-27 19:07:39 +0800

    redis unique queue improve

--- python/redis/unique_queue_2.py
+++ python/redis/unique_queue_2.py
@@ -12,15 +12,26 @@ else
 end
 '''
 
-r = redis.Redis()
-queue_pop = r.register_script(SCRIPT_POP)
+class UniqueQueue(object):
+
+    def __init__(self, key, r_host="127.0.0.1", r_port=6379, r_db=0):
+        self.mq_key = key
+        self.conn = redis.StrictRedis(r_host, r_port, db=r_db)
+        self.queue_pop = self.conn.register_script(SCRIPT_POP)
+
+    def pop(self):
+        return self.queue_pop(keys=[self.mq_key])
+
+    def push(self, task):
+        return self.conn.zadd(self.mq_key, time.time(), task)
+    
 
 if __name__ == "__main__":
-    mq_key = "ct:kafka:mq"
+    q = UniqueQueue("zset:mq:test")
     count = 10
     for i in range(count):
         for i in range(count):
-            r.zadd(mq_key, "Test_key_{}".format(i), time.time())
+            q.push("Test_key_{}".format(i))
 
     for i in range(count + 10):
-        print queue_pop(keys=[mq_key])
+        print q.pop()
